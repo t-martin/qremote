8f3d7da6fcaf2f6a224c0ae2dbb9f88a2d44db60
Tom Martin <tommartin7@gmail.com>
2019-10-15 15:14:23 +0100

    parse ini instead of xml

--- python/parse_config.py
+++ python/parse_config.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
 
+
+import configparser
 import argparse
-import xml.etree.ElementTree as ET
 
-def validate_option(option):
-  if (option != 'all' and not option.startswith('alias=')):
-    print(argparse.ArgumentTypeError("error: option must be either 'all' or 'alias=X'\n"))
-    arg_parse.print_help()
-    exit(1)
-  return option
-
-arg_parser = argparse.ArgumentParser(description = "xml config parser for qremote")
+arg_parser = argparse.ArgumentParser(description = "ini config parser for qremote")
 arg_parser.add_argument('config', help = "path to xml config file")
-arg_parser.add_argument('option', type = validate_option, help = "'all' to print all connection aliases or 'alias=X' to get the config for X")
+arg_parser.add_argument('option', help = "name of connection")
 args = arg_parser.parse_args()
 
-tree = ET.parse(args.config)
-root = tree.getroot()
-
+config = configparser.ConfigParser()
+config.read(args.config)
 if (args.option == 'all'):
-  xpath = "./connection/[@name]"
-  for elem in root.findall(xpath):
-    print(elem.attrib['name'])
-elif (args.option.startswith('alias=')):
-  alias = args.option.split('=')[1]
-  xpath = "./connection/[@name='%s']//" % alias
-  for elem in root.findall(xpath):
-    if(elem.text):
-      print(elem.tag + '=' + elem.text)
-else:
-  arg_parser.print_help()
- 
-
+    for section in config.sections():
+        print(section)
+else: 
+    for key in config[args.option]:
+        print(key + "=" + config[args.option][key])
